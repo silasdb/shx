360305750893848db6f8f13cfcac4a43dfb36b32
Silas Silva <silasdb@gmail.com>
2012-06-23 19:54:28 -0300

    Create shx_fatal_d function
    The difference between shx_fatal and shx_fatal_d is that the the second,
    prints a tip for debugging the script.

--- shx_log.sh
+++ shx_log.sh
@@ -6,8 +6,15 @@ shx_log_loaded="yes"
 shx_fatal ()
 {
 	echo "$*" >&2
-	echo "You may want use \"-! d\" to debug." >&2
 	shx_exit 1
+}
+
+# Outputs a message to stderr, a debug message and then exit with error
+shx_fatal_d ()
+{
+	echo "shx: You may want use \"-! d\" to debug your script." >&2
+	shx_fatal "$*"
+	# NOT REACHED
 }
 
 shx_warn ()
