c36580f1474aac486e39189376c78c4e1979d297
Silas Silva <silasdb@gmail.com>
2014-06-21 00:26:12 -0300

    Use printf instead of echo for security reasons
    Check comment at the top of the file and also
    http://www.dwheeler.com/essays/filenames-in-shell.html

--- shx_escape.sh
+++ shx_escape.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
 # Escapes a string so the shell can handle it.
+#
+# Realize that we do not use echo, but printf, which is more secure, since echo
+# handles escape characters and dashes in a way difficult to make sure it will
+# always work.  Check http://www.dwheeler.com/essays/filenames-in-shell.html for
+# more information.
 
 shx_escape_loaded="yes"
 
@@ -29,16 +34,10 @@ shx_escape ()
 	shx_priv_add_backslash '?'
 	shx_priv_add_backslash "'"
 	shx_priv_add_backslash '"'
-	shx_priv_echo "$shx_priv_e"
-}
-
-# Force escaping all options echo accepts (like -n and sometimes -e)
-shx_priv_echo ()
-{
-	echo -- "$@" | sed 's/-- //'
+	printf '%s' "$shx_priv_e"
 }
 
 shx_priv_add_backslash ()
 {
-	shx_priv_e=`shx_priv_echo "$shx_priv_e" | sed "s/$1/\\\\\\\\$1/g"`
+	shx_priv_e=$(printf '%s' "$shx_priv_e" | sed "s/$1/\\\\$1/g")
 }
