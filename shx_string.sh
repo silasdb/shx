ac05719f4c9c6bd2bc5b8403baba38c2ad668584
Silas Silva <silasdb@gmail.com>
2014-08-14 14:51:32 -0300

    Disable debugging for shx_escape
    Because it generates too much rarely useful debug information.

--- shx_string.sh
+++ shx_string.sh
@@ -4,9 +4,12 @@ shx_string_loaded="yes"
 
 shx_priv_e=
 
-# Escapes a string so the shell can handle it.
+# Escapes a string so the shell can handle it without quotes.
 shx_escape ()
 {
+	local old_opts
+	old_opts="$(set +o)"
+	set +x
 	shx_priv_e="$*"
 
 	# This must be before everything else
@@ -30,6 +33,7 @@ shx_escape ()
 	shx_priv_add_backslash '"'
 	shx_priv_add_backslash ';'
 	printf '%s' "$shx_priv_e"
+	eval "$old_opts" 2>/dev/null
 }
 
 shx_priv_add_backslash ()
