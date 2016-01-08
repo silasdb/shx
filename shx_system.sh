29c7761bc3788d6392f33bc91c3bc774795e5faa
Silas Silva <silasdb@gmail.com>
2016-01-08 14:21:21 -0200

    Add shx_md5 and shx_filesize functions
    (It lacks comments and a better organization)

--- shx_system.sh
+++ shx_system.sh
@@ -2,6 +2,8 @@
 
 shx_system_loaded="yes"
 
+shx_priv_md5cmd=
+
 # Checks if root is running the script. If it is not, outputs an error message
 # and exits.
 shx_onlyroot ()
@@ -20,4 +22,43 @@ shx_depends ()
 			return 3
 		fi
 	done
+}
+
+# Returns the md5 (the md5 only) for a given file or stdin.
+#
+# $1 -> The file.  stdin if ommited.
+shx_md5 ()
+{
+	if [ -z "$shx_priv_md5cmd" ]; then
+		shx_priv_set_md5cmd || return 1
+	fi
+
+	$shx_priv_md5cmd "$@" | awk '{ print $1; }'
+}
+
+shx_filesize ()
+{
+	wc -c "$1" | awk '{ print $1; }'
+	# TODO: wc doesn't work for file it cannot read.  Besides wc, having non
+	# portable "stat" might be a good idea.
+}
+
+shx_priv_set_md5cmd ()
+{
+	local x
+
+	x="$(which md5)"
+	if [ -n "$x" ]; then
+		shx_priv_md5cmd="md5 -n"
+		return 0
+	fi
+
+	x="$(which md5sum)"
+	if [ -n "$x" ]; then
+		shx_priv_md5cmd="md5sum"
+		return 0
+	fi
+
+	shx_warnln "No md5 program found."
+	return 1
 }
---
+++ tests/t_shx_system.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env atf-sh
+
+. "$(pkg-config --variable=prefix shx)"/shx_system.sh
+
+md5file_check ()
+{
+	local a
+	local b
+	a="$(md5 -n "$1" | awk '{ print $1; }')"
+	b="$(shx_md5 "$1")"
+
+	if [ "$a" != "$b" ]; then
+		atf_fail "$a != $b"
+	fi
+}
+
+md5stdin_check ()
+{
+	local a
+	local b
+	a="$(md5 -n "$1" | awk '{ print $1; }')"
+	b="$(cat "$1" | shx_md5)"
+
+	if [ "$a" != "$b" ]; then
+		atf_fail "$a != $b"
+	fi
+}
+
+atf_test_case md5
+md5
+{
+	atf_set  "descr"  "Test shx_md5 function"
+}
+md5_body ()
+{
+	md5file_check '/etc/fstab'
+}
+
+# TODO: add a test for md5 via stdin
+
+atf_init_test_cases ()
+{
+	atf_add_test_case md5
+}
