72e825f2e97d6a8f076431e88fc8f9f359cac794
Silas Silva <silasdb@gmail.com>
2016-02-13 21:34:48 -0200

    Add shx_field and shx_is_decimal functions
    Make unit tests too

--- shx_string.sh
+++ shx_string.sh
@@ -30,3 +30,52 @@ shx_quote ()
 	printf '%s' "'$q'"
 	eval "$old_opts" 2>/dev/null
 }
+
+# Prints the given field of a string.
+#
+# This function accepts two forms, reading the string from stdin or passing it
+# as the second parameter.  Fields are strings separated by spaces.  If the user
+# wants the 3rd word of the following string: "foo bar baz yyy", it may use one
+# of the forms:
+#
+#    echo "foo bar baz yyy" | shx_field 3
+#    shx_field 3 "foo bar baz yyy"
+#
+# $1 -> Field to be retrieved
+# $2 -> String.  Optional.  If ommited, it is read from stdin.
+#
+# Returns 0 if ok, 1 if the first parameter is not a decimal integer.
+shx_field ()
+{
+	if [ $# -eq 2 ]; then
+		# If the string were passed to us, we reset positional
+		# parameters, so we avoid forking a new awk.
+		local n="$1"
+		# TODO: we should check if it is positive too.
+		shx_is_decimal "$n" || return 1
+		# We need to set -f in order to prevent glob expansion when
+		# passing $@.
+		local old_opts
+		old_opts="$(set +o)"
+		set -f
+		set -- $@
+		shift
+		eval "shx_echo \$$n"
+		# Return shell options back (revert set -f).
+		eval "$old_opts" 2>/dev/null
+	else
+		awk "{ print \$$1 }"
+	fi
+}
+
+# Test if the string is a decimal integer.
+#
+# $1 -> String to check.
+#
+# Returns: 0, if it is a decimal integer, 1 otherwise.
+shx_is_decimal ()
+{
+	# Thanks to
+	# http://stackoverflow.com/questions/13147038/check-if-the-argument-is-a-number-inside-the-shell-script
+	(test "$1" -ge 0 || test "$1" -lt 0) 2>/dev/null || return 1
+}
--- tests/t_shx_string.sh
+++ tests/t_shx_string.sh
@@ -104,6 +104,80 @@ special_body ()
 	quote_check '\ <\ )$\\'		"'\\ <\\ )\$\\\\'"
 }
 
+
+# Test the shx_field function.
+# $1 -> field to try to pick out
+# $2 -> the string
+# $3 -> expected result
+field_check ()
+{
+	local res
+	# Test first form of shx_field
+	res="$(shx_field "$1" "$2")"
+	if [ "$res" != "$3" ]; then
+		atf_fail "$res != $3"
+	fi
+	# Test second form of shx_field
+	res="$(echo "$2" | shx_field "$1")"
+	if [ "$res" != "$3" ]; then
+		atf_fail "$res != $3"
+	fi
+}
+field_head ()
+{
+	atf_set 'descr'  'Test the shx_field function'
+}
+field_body ()
+{
+	field_check 1 'foo bar more'	'foo'
+	field_check 2 'foo bar more'	'bar'
+	field_check 3 'foo bar more'	'more'
+	field_check 1 'xx * ff'		'xx'
+	field_check 2 'xx * ff'		'*'
+	field_check 3 'xx * ff'		'ff'
+	field_check 1 'xx * [ab] ff'	'xx'
+	field_check 2 'xx * [ab] ff'	'*'
+	field_check 3 'xx * [ab] ff'	'[ab]'
+	field_check 1 'xx * [ab] ff'	'xx'
+	field_check 2 'xx * [ab] ff'	'*'
+	field_check 1 '/etc/* a b'	'/etc/*'
+	field_check 2 '/etc/* a b'	'a'
+}
+
+# Test the shx_is_decimal function.
+# $1 -> field to test
+# $2 -> fail or pass?
+is_decimal_check ()
+{
+	if [ "$2" = "pass" ]; then
+		shx_is_decimal "$1" || atf_fail "$1 not integer"
+	else
+		shx_is_decimal "$1" && atf_fail "$1 is integer"
+		# ATF doesn't allow us to return from a test with a non-ok exit
+		# status.
+		return 0
+	fi
+}
+
+is_decimal_head ()
+{
+	atf_set 'descr'  'Test the shx_is_decimal function'
+}
+is_decimal_body ()
+{
+	is_decimal_check 1		pass
+	is_decimal_check 10		pass
+	is_decimal_check 010		pass
+	is_decimal_check 239482		pass
+	is_decimal_check '-342'		pass
+	is_decimal_check 239.482	fail
+	is_decimal_check 239,482	fail
+	is_decimal_check 'sfdsf'	fail
+	is_decimal_check /etc/*		fail
+	is_decimal_check 12x324		fail
+	is_decimal_check 0xffee		fail
+}
+
 atf_init_test_cases ()
 {
 	atf_add_test_case basic
@@ -111,4 +185,6 @@ atf_init_test_cases ()
 	atf_add_test_case dashes
 	atf_add_test_case quotes
 	atf_add_test_case special
+	atf_add_test_case field
+	atf_add_test_case is_decimal
 }
